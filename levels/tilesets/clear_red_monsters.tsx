2cdeff5be8e734e5a95b1bf33c6216969ef0db82
Grace VB <89794102+code-gnome1@users.noreply.github.com>
2023-12-06 10:36:18 -0500

    Add files via upload

---
+++ levels/tilesets/clear_red_monsters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="clear_red_monsters" tilewidth="32" tileheight="32" tilecount="81" columns="9">
+ <image source="../../graphics/enemies/clear_red_monsters.png" width="288" height="288"/>
+</tileset>
---
+++ levels/tilesets/terrain_tiles2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="terrain_tiles2" tilewidth="32" tileheight="32" tilecount="783" columns="29">
+ <image source="../../graphics/terrain_tiles2.png" width="928" height="864"/>
+</tileset>
