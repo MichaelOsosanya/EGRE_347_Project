66bb88aa24c1d3d559d0eed32e551c4f4f289426
Grace VB <89794102+code-gnome1@users.noreply.github.com>
2023-12-06 10:56:31 -0500

    Add files via upload

---
+++ game_data.py
@@ -0,0 +1,6 @@
+sky_island = {
+    'terrain' : '../levels/sky_island/sky_island_Terrain.csv',
+    'decoration' : '../levels/sky_island/sky_island_Decoration.csv',
+    'enemies' : '../levels/sky_island/sky_island_Enemy.csv',
+    'player' : '../levels/sky_island/sky_island_Player.csv',
+    'background' : '../levels/sky_island/sky_island_Background.csv'}
