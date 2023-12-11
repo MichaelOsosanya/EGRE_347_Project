a5fca0e4174a883e2b9d766d2d77e76a5ba5c7b2
Grace VB <89794102+code-gnome1@users.noreply.github.com>
2023-12-11 14:13:45 -0500

    Update main.py
    Updated main.py to allow health bar to take input from the collision count and subtract 10 damage points every time player is shot

--- code/main.py
+++ code/main.py
@@ -4,12 +4,14 @@ from level import Level
 from game_data import sky_island
 from UI import UI
 from player import Player
+#from level import collision_count
 
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 level = Level( sky_island,screen)
 ui = UI(screen)
+collisionCount = level.collision_count
 
 while True:
     # update and draw sprites
@@ -21,6 +23,10 @@ while True:
     screen.fill('grey')
     #player.health_bar.draw(screen)
     level.run()
-    ui.show_health(50,100)
+    #print ("Collsion count is: ", level.collision_count)
+    damage_taken = level.collision_count * 10  #player damage is 10
+    current_health = 100 - damage_taken      #player health is 100
+   # print("current_health is ", current_health)
+    ui.show_health(current_health,100)
     pygame.display.update()
     clock.tick(60)
