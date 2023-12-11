48860315ae20bc3b2fcac13d90feb519561bb59c
rithikm <69921741+rithikm@users.noreply.github.com>
2023-12-11 00:05:09 -0500

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -2,20 +2,25 @@ import pygame, sys
 from settings import *
 from level import Level
 from game_data import sky_island
+from UI import UI
+from player import Player
 
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 level = Level( sky_island,screen)
+ui = UI(screen)
 
 while True:
+    # update and draw sprites
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
     screen.fill('grey')
+    #player.health_bar.draw(screen)
     level.run()
-
+    ui.show_health(50,100)
     pygame.display.update()
     clock.tick(60)
